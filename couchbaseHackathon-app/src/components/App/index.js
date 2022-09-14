6849c9443e75b2eb9835285f16b69dc2fbca54fe
hkmarcoot <59559509+hkmarcoot@users.noreply.github.com>
2022-09-14 23:28:39 +0100

    add Couchbase hackathon app

---
+++ couchbaseHackathon-app/src/components/App/index.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import Cards from "../Cards";
+import "../../styles.css";
+
+const API_URL = "http://localhost:3000";
+
+function App() {
+  const [lostitem, setLostitem] = useState([]);
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState("");
+  const [isPending, setIsPending] = useState(true);
+
+
+  useEffect(() => {
+    async function getlostitem() {
+      await fetch(`${API_URL}/lostitem`, { referrerPolicy: "no-referrer" })
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("could not fetch the data for for that resourse");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          //console.log(data);
+          setIsPending(false);
+          setLostitem(data);
+          setError(null);
+        })
+        .catch((err) => {
+          //auto catches network / connection error
+          setIsPending(false);
+          setError(err.message);
+        });
+    }
+    getlostitem();
+    console.log("old" + lostitem);
+  }, []);
+  console.log(lostitem);
+  return (<><div className="titles">LOST AND FOUND</div>
+  <div>{lostitem && <Cards lostitem={lostitem} />}</div>
+  </>);
+}
+
+export default App;
