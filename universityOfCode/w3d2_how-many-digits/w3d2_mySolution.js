cfb079d98d0f95abf33e5b4192a45496448738fd
hkmarcoot <59559509+hkmarcoot@users.noreply.github.com>
2023-05-16 20:00:35 +0100

    add w3d2 UOC daily coding challenge

---
+++ universityOfCode/w3d2_how-many-digits/w3d2_mySolution.js
@@ -0,0 +1,7 @@
+/*************Code Starts Here*****************/
+function digits(x) {
+  return Math.max(Math.floor(Math.log10(Math.abs(x))), 0) + 1;
+}
+/********Code Ends*****************/
+//   Reference:
+//   https://stackoverflow.com/questions/14879691/get-number-of-digits-with-javascript
