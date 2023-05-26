c69620995fc9205c533178d69acf3e2a3a22e7b3
hkmarcoot <59559509+hkmarcoot@users.noreply.github.com>
2023-05-26 02:02:36 +0100

    add w4d5 UOC daily coding challenge

---
+++ universityOfCode/w4d5_only-numbers-allowed/w4d5_mySolution.js
@@ -0,0 +1,8 @@
+function numOnly(arr) {
+  return arr.filter((x) => /\d/.test(x));
+}
+
+// Reference:
+// https://codingbeautydev.com/blog/javascript-check-if-string-contains-numbers/
+// https://stackoverflow.com/questions/26716818/using-array-map-to-filter-results-with-if-conditional
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
