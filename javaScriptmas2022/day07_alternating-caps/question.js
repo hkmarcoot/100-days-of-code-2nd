addba15143834f117886dfb681e63f34f7469aec
hkmarcoot <59559509+hkmarcoot@users.noreply.github.com>
2023-05-29 16:22:08 +0100

    add javaScriptmas 2022 day 7 alternating caps

---
+++ javaScriptmas2022/day07_alternating-caps/question.js
@@ -0,0 +1,16 @@
+/* Alternating Caps 
+ Write a function that takes in a string of letters
+ and returns a sentence in which every other letter is capitalized.
+
+Example input: "I'm so happy it's Monday"
+Example output: "I'M So hApPy iT'S MoNdAy"
+*/
+
+function altCaps(str) {
+  return newStr;
+}
+
+console.log(altCaps("When you visit Portland you have to go to VooDoo Donuts"));
+
+// Link
+// https://scrimba.com/learn/javascriptmas/🔡-Alternating-Caps-cwGqbBtz?utm_source=convertkit&utm_medium=email&utm_campaign=🎄+JavaScriptmas%3A+Your+Day+7+Challenge+-9580149
