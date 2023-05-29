6203cd7480b0ddd9eed578c38db1c12b8bc502b9
hkmarcoot <59559509+hkmarcoot@users.noreply.github.com>
2023-05-29 16:05:22 +0100

    add javaScriptmas 2022 day 2 totally private data farm

---
+++ javaScriptmas2022/day02_totally-private-data-farm/question.js
@@ -0,0 +1,32 @@
+import userData from "./data.js";
+
+/* Totally Private Data Farm 
+
+Good news, renown advertising firm Evil Corp. wants to purchase our 
+private user data! 
+
+We'd never do this in real life of course, but just for practice 
+let's pretend we're unethical web hackers and transform the data 
+in the way Evil Corp. has requested. They're quite particular and
+just want an array of users with a fullname and human readable
+birthday.   
+
+Write a function that maps through the current data and returns
+a new an array of objects with only two properties: 
+fullName and birthday. Each result in your 
+array should look like this when you're done: 
+
+{
+    fullName: "Levent Busser", 
+    birthday: "Fri Aug 20 1971"
+}
+
+Read about toDateString() for info on formatting a readable date. 
+
+*/
+function transformData(data) {}
+
+console.log(transformData(userData));
+
+// Link
+// https://scrimba.com/learn/javascriptmas/🕵️-Totally-Private-Data-Farm-c2y2yBAV?utm_source=convertkit&utm_medium=email&utm_campaign=🎄+JavaScriptmas%3A+Your+Day+2+Challenge+-9546979
