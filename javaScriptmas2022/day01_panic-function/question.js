7b12954accb244e764e242ac7a5026f884172742
hkmarcoot <59559509+hkmarcoot@users.noreply.github.com>
2023-05-29 16:00:47 +0100

    add javaScriptmas 2022 day 1 panic function

---
+++ javaScriptmas2022/day01_panic-function/question.js
@@ -0,0 +1,20 @@
+/* Panic function 
+Write a PANIC! function. The function should take in a sentence and return the same
+sentence in all caps with an exclamation point (!) at the end. Use JavaScript's
+built in string methods. 
+
+If the string is a phrase or sentence, add a 😱 emoji in between each word. 
+
+Example input: "Hello"
+Example output: "HELLO!"
+
+Example input: "I'm almost out of coffee"
+Example output: "I'M 😱 ALMOST 😱 OUT 😱 OF 😱 COFFEE!"
+*/
+
+// Test your function
+console.log(panic("I'm almost out of coffee"));
+console.log(panic("winter is coming"));
+
+// Link
+// https://scrimba.com/learn/javascriptmas/😱-Panic-Function-c6w3DbAG?utm_source=convertkit&utm_medium=email&utm_campaign=🎄+JavaScriptmas%3A+24+code+challenges! - 9543182
