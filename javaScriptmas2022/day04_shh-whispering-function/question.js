098a7442215e25b4d2d2e4d30728e359104dd26c
hkmarcoot <59559509+hkmarcoot@users.noreply.github.com>
2023-05-29 16:12:45 +0100

    add javaScriptmas 2022 day 4 shh whispering function

---
+++ javaScriptmas2022/day04_shh-whispering-function/question.js
@@ -0,0 +1,20 @@
+/* Whispering function 
+Write a function `whisper` that takes in a sentence 
+and returns a new sentence in all lowercase letters with
+"shh..." at the beginning. 
+
+The function should also remove an exclamation point
+at the end of the sentence, if there is one. 
+
+Example 
+input: "The KITTENS are SLEEPING!"
+output: "shh... the kittens are sleeping"
+
+Hint: endsWith and slice
+*/
+
+console.log(whisper("PLEASE STOP SHOUTING."));
+console.log(whisper("MA'AM, this is a Wendy's!"));
+
+// Link
+// https://scrimba.com/learn/javascriptmas/🤫-Shh...-Whispering-Function-cof13499292ecbde5bab60c5f?utm_source=convertkit&utm_medium=email&utm_campaign=🎄+JavaScriptmas%3A+Your+Day+4+Challenge+%20-%209559000
