cd2b29055cdc328ec51af24f0c36de38081466c2
hkmarcoot <59559509+hkmarcoot@users.noreply.github.com>
2023-06-15 12:40:10 +0100

    add UOC daily coding challenge w7d4 sorting the impossible

---
+++ universityOfCode/w7d4_sorting-the-impossible/w7d4_mySolution.js
@@ -0,0 +1,12 @@
+function sorting(arr) {
+  return arr.sort((a, b) => a - b);
+}
+
+console.log(sorting([4, [1], 3]));
+
+/* Given Helpful Links
+ Array.prototype.sort()
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+ Array.prototype.flat()
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
+*/
