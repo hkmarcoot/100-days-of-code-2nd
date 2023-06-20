6bd21e2085735809f6779d001b57ca7f537e5724
hkmarcoot <59559509+hkmarcoot@users.noreply.github.com>
2023-06-20 13:03:24 +0100

    add UOC daily coding challenge w8d2 no zeroes

---
+++ universityOfCode/w8d2_no-zeroes/w8d2_mySolution.js
@@ -0,0 +1,16 @@
+function noZeroes(str) {
+  return parseFloat(str).toString();
+}
+
+/* Given Helpful Links
+ parseFloat()
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseFloat
+ Math.abs()
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
+ Number Object
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
+ Object.prototype.toString()
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/toString
+ Regular Expressions
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
+*/
