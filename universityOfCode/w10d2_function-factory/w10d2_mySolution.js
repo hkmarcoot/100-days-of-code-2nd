2c472d9072b1ad9351ffe1d3eafeb715d57ddde1
hkmarcoot <59559509+hkmarcoot@users.noreply.github.com>
2023-07-04 15:50:08 +0100

    add UOC daily coding challenge w10d2 function factory

---
+++ universityOfCode/w10d2_function-factory/w10d2_mySolution.js
@@ -0,0 +1,23 @@
+function makePlusFunction(num1) {
+  return function (num2) {
+    return num1 + num2;
+  };
+}
+
+const plusTwo = makePlusFunction(2);
+const plusFive = makePlusFunction(5);
+const plusSeven = makePlusFunction(plusTwo(plusFive(0)));
+const plusTen = makePlusFunction(10);
+
+console.log(plusFive(2));
+console.log(plusFive(-8));
+console.log(plusTwo(0));
+console.log(plusTwo(18));
+console.log(plusTwo(plusFive(plusSeven(plusTen(1)))));
+console.log(makePlusFunction(8)(8));
+console.log(makePlusFunction(1)(100));
+console.log(makePlusFunction(-100)(0));
+console.log(makePlusFunction(0)(0));
+
+// Reference
+// Review w3d1 Same Function Again
