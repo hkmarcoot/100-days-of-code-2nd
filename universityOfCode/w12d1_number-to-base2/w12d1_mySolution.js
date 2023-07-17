62068e233d174ad63779b618d6a96fa33f70ee95
hkmarcoot <59559509+hkmarcoot@users.noreply.github.com>
2023-07-17 15:18:26 +0100

    add UOC daily coding challenge w12d1 number to base2

---
+++ universityOfCode/w12d1_number-to-base2/w12d1_mySolution.js
@@ -0,0 +1,27 @@
+function binary(num) {
+  return parseInt(num, 10).toString(2);
+}
+
+console.log(binary(100));
+console.log(binary(1));
+console.log(binary(0));
+console.log(binary(69));
+console.log(binary(1023));
+console.log(binary(511));
+console.log(binary(666));
+console.log(binary(123));
+
+/* Given Helpful Links
+ Decimal to Binary Converter
+   - https://www.rapidtables.com/convert/number/decimal-to-binary.html
+ Writing Binary Numbers
+   - https://wild.maths.org/writing-binary-numbers
+ Convert a Decimal Number to Binary, Hexadecimal or Octal
+   - https://www.w3resource.com/javascript-exercises/javascript-math-exercise-3.php
+ Convert to Binary
+   - https://www.w3resource.com/javascript-exercises/javascript-math-exercise-3.php
+ How To Convert Decimal to Binary
+   - https://www.youtube.com/watch?v=rsxT4FfRBaM&t=27s
+ Number.prototype.toString()
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toString
+*/
