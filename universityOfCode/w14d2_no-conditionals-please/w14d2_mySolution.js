847b334c46ee84b492770d4407fbb1f6fc26b62e
hkmarcoot <59559509+hkmarcoot@users.noreply.github.com>
2023-08-01 13:20:22 +0100

    add UOC daily coding challenge w14d2 no conditionals please

---
+++ universityOfCode/w14d2_no-conditionals-please/w14d2_mySolution.js
@@ -0,0 +1,13 @@
+function flip(num) {
+  return Math.abs(num - 1);
+}
+
+console.log(flip(0));
+console.log(flip(1));
+
+/* Given Helpful Links
+ Math.abs()
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/abs
+ Subtraction (-)
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Subtraction
+*/
