3e2af5fbcda93962f81b251637f234309913df66
hkmarcoot <59559509+hkmarcoot@users.noreply.github.com>
2023-08-31 00:44:49 +0100

    add UOC daily coding challenge w18d4 derivative function

---
+++ universityOfCode/w18d4_derivative-function/w18d4_mySolution.js
@@ -0,0 +1,18 @@
+function derivative(b, m) {
+  return b * Math.pow(m, b - 1);
+}
+
+console.log(derivative(1, 4));
+console.log(derivative(3, -2));
+console.log(derivative(4, -3));
+
+/* Given Helpful Links
+ Introduction to Derivatives
+   - https://www.mathsisfun.com/calculus/derivatives-introduction.html
+ Power Rule
+   - https://www.mathsisfun.com/calculus/power-rule.html
+ Math.pow() Method
+   - https://www.w3schools.com/jsref/jsref_pow.asp
+ Formula to Find the Derivative of a Function
+   - https://www.youtube.com/watch?v=pBc4Udqw330
+*/
