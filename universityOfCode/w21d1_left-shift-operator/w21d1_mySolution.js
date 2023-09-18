7b53eca43e0680a12d1d427bb1136d01749e4c1e
hkmarcoot <59559509+hkmarcoot@users.noreply.github.com>
2023-09-18 18:33:22 +0100

    add UOC daily coding challenge w21d1 left shift operator

---
+++ universityOfCode/w21d1_left-shift-operator/w21d1_mySolution.js
@@ -0,0 +1,12 @@
+function shiftLeft(num, y) {
+  return num * Math.pow(2, y);
+}
+console.log(shiftLeft(-32, 2));
+console.log(shiftLeft(-6, 5));
+console.log(shiftLeft(12, 4));
+console.log(shiftLeft(46, 6));
+
+/* Given Helpful Links
+ Left shift (<<)
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Left_shift
+*/
