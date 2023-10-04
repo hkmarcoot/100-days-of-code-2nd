28293257caf2ad7c13238c656544c21f9813c4a4
hkmarcoot <59559509+hkmarcoot@users.noreply.github.com>
2023-10-04 17:23:01 +0100

    add UOC daily coding challenge w23d3 fix the code

---
+++ universityOfCode/w23d3_fix-the-code/w23d3_mySolution.js
@@ -0,0 +1,12 @@
+function clone(arr) {
+  return [...arr, arr];
+}
+
+console.log(clone([1, 2, 3]));
+
+/* Given Helpful Links
+ Spread syntax (...)
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+ slice() Method
+   - https://www.w3schools.com/jsref/jsref_slice_string.asp
+*/
