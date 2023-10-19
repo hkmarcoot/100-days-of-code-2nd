2ff460ab16666f2f0034ddb6c5dc6599a8978651
hkmarcoot <59559509+hkmarcoot@users.noreply.github.com>
2023-10-19 17:35:47 +0100

    add helpful links for w25d3 repdigit

--- universityOfCode/w25d3_repdigit/w25d3_mySolution.js
+++ universityOfCode/w25d3_repdigit/w25d3_mySolution.js
@@ -14,3 +14,16 @@ console.log(isRepdigit(0));
 console.log(isRepdigit(-11));
 console.log(isRepdigit(6666));
 console.log(isRepdigit(1001));
+
+/* Given Helpful Links
+ Repdigit
+   - https://en.wikipedia.org/wiki/Repdigit
+ Set
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+ Array.prototype.every()
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+ Remainder (%)
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators#Remainder_()
+ String.prototype.split()
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+*/
