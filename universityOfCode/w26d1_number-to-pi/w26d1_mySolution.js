9b5b30adb6bbdb9835123d4f2e35748916942cb9
hkmarcoot <59559509+hkmarcoot@users.noreply.github.com>
2023-10-23 14:34:44 +0100

    Add UOC daily coding challenge w26d1 number to pi

---
+++ universityOfCode/w26d1_number-to-pi/w26d1_mySolution.js
@@ -0,0 +1,16 @@
+function myPi(n) {
+  return parseFloat(Math.PI.toFixed(n));
+}
+
+console.log(myPi(5));
+console.log(myPi(4));
+console.log(myPi(15));
+
+/* Given Helpful Links
+ Number.prototype.toFixed()
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
+ Math.PI
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/PI
+ Number.parseFloat()
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/parseFloat
+*/
