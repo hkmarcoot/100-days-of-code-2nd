3e4631cb750732c98ca800c86d9b333a3ceb5ade
hkmarcoot <59559509+hkmarcoot@users.noreply.github.com>
2023-10-25 14:39:07 +0100

    Add UOC daily coding challenge w26d3 trace that matrix

---
+++ universityOfCode/w26d3_trace-that-matrix/w26d3_mySolution.js
@@ -0,0 +1,57 @@
+function trace(arr) {
+  var sum = 0;
+  for (var i = 0; i < arr[0].length; i++) {
+    sum += arr[i][i];
+  }
+  return sum;
+}
+
+console.log(
+  trace([
+    [1, 4],
+    [4, 1],
+  ])
+);
+console.log(
+  trace([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ])
+);
+console.log(trace([[12345]]));
+console.log(
+  trace([
+    [1, 0, 1, 0],
+    [0, 2, 0, 2],
+    [3, 0, 3, 0],
+    [0, 4, 0, 4],
+  ])
+);
+console.log(
+  trace([
+    [0, 1, 0, 1],
+    [2, 0, 2, 0],
+    [0, 3, 0, 3],
+    [4, 0, 4, 0],
+  ])
+);
+console.log(
+  trace([
+    [1, 8, 9, 16],
+    [2, 7, 10, 15],
+    [3, 6, 11, 14],
+    [4, 5, 12, 13],
+  ])
+);
+
+/* Given Helpful Links
+ Array.prototype.reduce()
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+ Array.prototype.map()
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+ Array.prototype.filter()
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+ for...in
+   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
+*/
