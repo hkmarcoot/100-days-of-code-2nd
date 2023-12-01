de6479848950a76cf46732420e6edbe928913b83
hkmarcoot <59559509+hkmarcoot@users.noreply.github.com>
2023-12-01 15:15:05 +0000

    Add UOC daily coding challenge w31d5 pentagonal number

---
+++ universityOfCode/w31d5_pentagonal-number/w31d5_mySolution.js
@@ -0,0 +1,13 @@
+function pentagonal(num) {
+  return (5 * num * num - 5 * num + 2) / 2;
+}
+
+console.log(pentagonal(1));
+console.log(pentagonal(2));
+console.log(pentagonal(3));
+console.log(pentagonal(4));
+
+/* Given Helpful Links
+ Centered Pentagonal Number 
+   - https://en.wikipedia.org/wiki/Centered_pentagonal_number
+*/
