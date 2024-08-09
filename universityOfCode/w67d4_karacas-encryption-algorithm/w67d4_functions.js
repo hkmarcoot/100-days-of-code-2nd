2e4ef7c7612f990582525a239cbf707cc5582070
hkmarcoot <hkmarcoot@gmail.com>
2024-08-09 14:25:22 +0100

    Add helpful links to w67d4 karacas encryption algorithm

--- universityOfCode/w67d4_karacas-encryption-algorithm/w67d4_functions.js
+++ universityOfCode/w67d4_karacas-encryption-algorithm/w67d4_functions.js
@@ -12,3 +12,18 @@ module.exports = encrypt;
 // Reference
 // https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
 // https://www.w3schools.com/jsref/jsref_replace.asp
+
+/* Given Helpful Links
+   Array.prototype.join()
+     - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+   Array.prototype.reverse()
+     - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse
+   String.prototype.split()
+     - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+   Destructuring Assignment
+     - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+   String.prototype.replace()
+     - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+   RegExp Constructor
+     - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
+*/
