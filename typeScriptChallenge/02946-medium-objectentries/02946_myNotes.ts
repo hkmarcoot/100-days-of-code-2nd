def411ac9afcc26962aee1aec50bac95d1a523fb
hkmarcoot <hkmarcoot@gmail.com>
2024-09-19 12:18:58 +0100

    Add TypeScript Challenge 02946 medium-objectentries

---
+++ typeScriptChallenge/02946-medium-objectentries/02946_myNotes.ts
@@ -0,0 +1,68 @@
+/*
+  2946 - ObjectEntries
+  -------
+  by jiangshan (@jiangshanmeta) #medium #object
+
+  ### Question
+
+  Implement the type version of ```Object.entries```
+
+  For example
+
+  ```typescript
+  interface Model {
+    name: string;
+    age: number;
+    locations: string[] | null;
+  }
+  type modelEntries = ObjectEntries<Model> // ['name', string] | ['age', number] | ['locations', string[] | null];
+  ```
+
+  > View on GitHub: https://tsch.js.org/2946
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type RemoveUndefined<T> = [T] extends [undefined] ? T : Exclude<T, undefined>;
+
+type ObjectEntries<T> = RemoveUndefined<
+  {
+    [K in keyof T]: {} extends Pick<T, K>
+      ? [K, RemoveUndefined<T[K]>]
+      : [K, T[K]];
+  }[keyof T]
+>;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+interface Model {
+  name: string;
+  age: number;
+  locations: string[] | null;
+}
+
+type ModelEntries =
+  | ["name", string]
+  | ["age", number]
+  | ["locations", string[] | null];
+
+type cases = [
+  Expect<Equal<ObjectEntries<Model>, ModelEntries>>,
+  Expect<Equal<ObjectEntries<Partial<Model>>, ModelEntries>>,
+  Expect<Equal<ObjectEntries<{ key?: undefined }>, ["key", undefined]>>,
+  Expect<Equal<ObjectEntries<{ key: undefined }>, ["key", undefined]>>,
+  Expect<
+    Equal<
+      ObjectEntries<{ key: string | undefined }>,
+      ["key", string | undefined]
+    >
+  >
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/2946/answer
+  > View solutions: https://tsch.js.org/2946/solutions
+  > More Challenges: https://tsch.js.org
+*/
