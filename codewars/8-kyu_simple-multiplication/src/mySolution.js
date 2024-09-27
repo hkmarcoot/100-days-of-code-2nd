80752975effbd33b5782331b2b979639ce409646
hkmarcoot <hkmarcoot@gmail.com>
2024-09-27 14:33:08 +0100

    Add Codewars Challenge 8-kyu simple multiplication

---
+++ codewars/8-kyu_simple-multiplication/src/mySolution.js
@@ -0,0 +1,15 @@
+/*
+Title: 
+- Simple multiplication
+
+Description:
+- This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+Tags:
+- Fundamentals
+
+*/
+
+export function simpleMultiplication(number) {
+  return number % 2 === 0 ? number * 8 : number * 9;
+}
