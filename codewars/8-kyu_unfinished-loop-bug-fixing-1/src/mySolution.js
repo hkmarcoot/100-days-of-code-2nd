1d03a8a4ef970cf53a7464ebd20776a958315348
hkmarcoot <hkmarcoot@gmail.com>
2024-10-11 18:09:02 +0100

    Add Codewars Challenge 8-kyu unfinished loop bug fixing 1

---
+++ codewars/8-kyu_unfinished-loop-bug-fixing-1/src/mySolution.js
@@ -0,0 +1,22 @@
+/*
+Title: 
+- Unfinished Loop - Bug Fixing #1
+
+Description:
+- Unfinished Loop - Bug Fixing #1
+Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+Tags:
+- Debugging
+
+*/
+
+export function createArray(number) {
+  var newArray = [];
+
+  for (var counter = 1; counter <= number; counter++) {
+    newArray.push(counter);
+  }
+
+  return newArray;
+}
