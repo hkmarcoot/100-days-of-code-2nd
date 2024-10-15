dfc7749268e8102fc3c0e259bc66d854dc94770a
hkmarcoot <hkmarcoot@gmail.com>
2024-10-15 23:40:27 +0100

    Add Codecademy JS project dog years

---
+++ codecademy/full-stack-engineer-career-path/js-pt1-intro-project-2/dog-years.js
@@ -0,0 +1,18 @@
+// my age
+const myAge = 33;
+// early years
+let earlyYears = 2;
+// multiply by 10.5
+earlyYears = earlyYears * 10.5;
+// later years is my age - 2
+let laterYears = myAge - 2;
+// later years times 4
+laterYears *= 4;
+// adding together early years and later years
+let myAgeInDogYears = earlyYears + laterYears;
+// my name in lowercase
+let myName = "Marco".toLowerCase();
+// log the data
+console.log(
+  `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`
+);
