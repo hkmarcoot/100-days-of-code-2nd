243f4314cecc6cf946079ad2b65f39d310223e47
hkmarcoot <hkmarcoot@gmail.com>
2024-10-15 23:43:08 +0100

    Add Codecademy JS conditionals project magic eight ball

---
+++ codecademy/full-stack-engineer-career-path/js-pt1-conditionals-project-1/magic-eight-ball.js
@@ -0,0 +1,33 @@
+let userName = "";
+
+userName ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
+let userQuestion = "";
+console.log("You " + userName + "asked: " + userQuestion + ".");
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = "";
+switch (randomNumber) {
+  case 1:
+    console.log("It is certain");
+    break;
+  case 2:
+    console.log("It is decidedly so");
+    break;
+  case 3:
+    console.log("Reply hazy try again");
+    break;
+  case 4:
+    console.log("Cannot predict now");
+    break;
+  case 5:
+    console.log("Do not count on it");
+    break;
+  case 6:
+    console.log("My sources say no");
+    break;
+  case 7:
+    console.log("Outlook not so good");
+    break;
+  case 0:
+    console.log("Signs point to yes");
+    break;
+}
