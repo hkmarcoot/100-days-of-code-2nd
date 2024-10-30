b9108f9eba733b74bc375684e6e21e10d30b2a3e
hkmarcoot <hkmarcoot@gmail.com>
2024-10-30 00:21:50 +0000

    Add Codecademy classes example

---
+++ codecademy/full-stack-engineer-career-path/js-pt3-classes-example/01_intro-object.js
@@ -0,0 +1,16 @@
+let halley = {
+  _name: "Halley",
+  _behavior: 0,
+
+  get name() {
+    return this._name;
+  },
+
+  get behavior() {
+    return this._behavior;
+  },
+
+  incrementBehavior() {
+    this._behavior++;
+  },
+};
