9f5a0cf70182f9a81fa859985b6582397f74be30
hkmarcoot <hkmarcoot@gmail.com>
2024-11-14 01:53:29 +0000

    Add Codecademy Example - Components Render Other Components

---
+++ codecademy/full-stack-engineer-career-path/react-pt2-components-interacting/01_components-render-other-components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HelloWorld from "./HelloWorld";
+
+function App() {
+  return (
+    <div>
+      <HelloWorld />
+      <div>I am Marco</div>
+    </div>
+  );
+}
+
+export default App;
