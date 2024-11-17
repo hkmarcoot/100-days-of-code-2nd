83d2b42a69280613419f586a4ca6f6d057525d06
hkmarcoot <hkmarcoot@gmail.com>
2024-11-17 21:13:07 +0000

    Add Codecademy Project - Passing Thoughts

---
+++ codecademy/full-stack-engineer-career-path/react-pt2-hooks-project/passing-thoughts/AddThoughtForm.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { generateId, getNewExpirationTime } from "./utilities";
+
+export function AddThoughtForm(props) {
+  const [text, setText] = useState("");
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const obj = {
+      id: generateId(),
+      text: text,
+      expiresAt: getNewExpirationTime(),
+    };
+    if (text.length === 0) return;
+    props.addThought(obj);
+    setText("");
+  };
+  return (
+    <form className="AddThoughtForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        aria-label="What's on your mind?"
+        placeholder="What's on your mind?"
+        value={text}
+        onChange={handleTextChange}
+      />
+      <input type="submit" value="Add" />
+    </form>
+  );
+}
