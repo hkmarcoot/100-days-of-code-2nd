bbc977daf20704ea57dd6faba0b6ea84fe2b7ee9
hkmarcoot <hkmarcoot@gmail.com>
2024-11-21 02:27:19 +0000

    Complete Codecademy Project - Adopt a Pet

--- codecademy/full-stack-engineer-career-path/react-pt2-react-router-project/adopt-a-pet/src/App.js
+++ codecademy/full-stack-engineer-career-path/react-pt2-react-router-project/adopt-a-pet/src/App.js
@@ -1,18 +1,35 @@
-import HomePage from './pages/home';
-import SearchPage from './pages/search';
-import PetDetailsPage from './pages/detail';
-import PetDetailsNotFound from './pages/petDetailsNotFound';
-import Root from './components/root';
+import HomePage from "./pages/home";
+import SearchPage from "./pages/search";
+import PetDetailsPage from "./pages/detail";
+import PetDetailsNotFound from "./pages/petDetailsNotFound";
+import Root from "./components/root";
 
 // Add react-router-dom imports
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 
 // create router with JSX Route elements
-const appRouter = "REPLACE ME";
+const appRouter = createBrowserRouter(
+  createRoutesFromElements(
+    /* Wrap this Root Route to create Router here */
+    <Route path="/" element={<Root />}>
+      <Route index element={<HomePage />} />
+      <Route path=":type" element={<HomePage />} />
+      <Route path=":type/:id" element={<PetDetailsPage />} />
+      <Route path="search" element={<SearchPage />} />
+      <Route path="pet-details-not-found" element={<PetDetailsNotFound />} />
+    </Route>
+  )
+);
 
 function App() {
   return (
     // replace below with a Router Provider
-    <p>REPLACE ME</p>
+    <RouterProvider router={appRouter} />
   );
 }
 
