704fd6919f3535b461cad451d76cb2f30748eec0
hkmarcoot <hkmarcoot@gmail.com>
2024-11-22 00:45:32 +0000

    Add Codecademy Example - Matcher Functions

---
+++ codecademy/full-stack-engineer-career-path/react-pt2-testing-example/02_matcher-functions/language_spoken.test.js
@@ -0,0 +1,27 @@
+import {
+  capitalize,
+  getAlpha2Code,
+  countryExtractor,
+  countryListLookup,
+  getResponse,
+} from "./language_spoken.js";
+
+it("converts array of country data objects to array of countries", () => {
+  //arrange
+  const inputObject = [
+    { name: "Argentina", capital: "Buenos Aires" },
+    { name: "Belize", capital: "Belmopan" },
+    { name: "Bolivia", capital: "Sucre" },
+  ];
+  const expectedValue = ["Argentina", "Belize", "Bolivia"];
+
+  //act
+  const actualValue = countryExtractor(inputObject);
+
+  //assert
+  expect(actualValue).toEqual(expectedValue);
+  expect(actualValue[0]).toBe("Argentina");
+  expect(actualValue).toContain("Belize");
+  expect(actualValue[2] === "Bolivia").toBeTruthy();
+  expect(actualValue[3]).not.toBeDefined();
+});
