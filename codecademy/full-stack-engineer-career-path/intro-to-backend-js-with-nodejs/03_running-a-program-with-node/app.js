d766c3f2a3364e155acb8c480053389af8a6689a
hkmarcoot <hkmarcoot@gmail.com>
2024-12-04 22:28:07 +0000

    Add Codecademy Example - Running a Program with Node

---
+++ codecademy/full-stack-engineer-career-path/intro-to-backend-js-with-nodejs/03_running-a-program-with-node/app.js
@@ -0,0 +1,11 @@
+let noun1 = "orange";
+let adjective = "happy";
+let noun2 = "person";
+let verb = "laugh";
+let noun3 = "ice-cream";
+
+console.log(
+  `The world's first ${noun1} was a very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`
+);
+
+// run node app.js
