427157f45d89ccb3d9544df9fbfae53f9f26f84c
hkmarcoot <hkmarcoot@gmail.com>
2024-12-09 23:44:49 +0000

    Add Codecademy Example - Writing Your First Route

---
+++ codecademy/full-stack-engineer-career-path/build-a-backend-with-expressjs-routes-1/03_writing-your-first-route/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 4001;
+// Use static server to serve the Express Yourself Website
+app.use(express.static("public"));
+
+// Open a call to `app.get()` below:
+app.get("/expressions", (req, res, next) => {
+  // Here we would send back the moods array in response
+  console.log(req);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
