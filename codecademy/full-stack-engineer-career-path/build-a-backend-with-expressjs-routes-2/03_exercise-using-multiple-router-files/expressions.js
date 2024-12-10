36c1f5411f6f07d3202f0efcf250ce8d5bf18abf
hkmarcoot <hkmarcoot@gmail.com>
2024-12-10 21:05:12 +0000

    Add Codecademy Example - Exercise Using Multiple Router Files

---
+++ codecademy/full-stack-engineer-career-path/build-a-backend-with-expressjs-routes-2/03_exercise-using-multiple-router-files/expressions.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const {
+  seedElements,
+  getElementById,
+  createElement,
+  updateElement,
+  getIndexById,
+} = require("./utils");
+
+let expressions = [];
+seedElements(expressions, "expressions");
+
+const expressionsRouter = express.Router();
+
+module.exports = expressionsRouter;
+
+// Get all expressions
+expressionsRouter.get("/", (req, res, next) => {
+  res.send(expressions);
+});
