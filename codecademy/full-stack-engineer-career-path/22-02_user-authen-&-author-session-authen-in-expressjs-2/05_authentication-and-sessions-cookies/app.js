be7b37660118aba5abfb454d63c2aeedad452ad8
hkmarcoot <hkmarcoot@gmail.com>
2024-12-18 00:04:11 +0000

    Add Codecademy Example - Authentication and Sessions: Cookies

---
+++ codecademy/full-stack-engineer-career-path/22-02_user-authen-&-author-session-authen-in-expressjs-2/05_authentication-and-sessions-cookies/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const session = require("express-session");
+app.set("trust proxy", 1);
+const PORT = process.env.PORT || 4001;
+const store = new session.MemoryStore();
+
+app.use(
+  session({
+    secret: "D53gxl41G",
+    // Add the cookie property below:
+    cookie: { maxAge: 172800000, secure: true, sameSite: "none" },
+    resave: false,
+    saveUninitialized: false,
+    store,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
