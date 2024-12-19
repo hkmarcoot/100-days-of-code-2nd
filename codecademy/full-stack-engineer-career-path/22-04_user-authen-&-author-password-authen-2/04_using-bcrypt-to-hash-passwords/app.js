2471e858bbef465c2965f92cf18230756a99a9c6
hkmarcoot <hkmarcoot@gmail.com>
2024-12-19 21:44:43 +0000

    Add Codecademy Example - Using bcrypt to Hash Passwords

---
+++ codecademy/full-stack-engineer-career-path/22-04_user-authen-&-author-password-authen-2/04_using-bcrypt-to-hash-passwords/app.js
@@ -0,0 +1,13 @@
+const bcrypt = require("bcrypt");
+
+// Create password hashing function below:
+const passwordHash = async (password, saltRounds) => {
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
+  } catch (err) {
+    console.log(err);
+  }
+  return null;
+};
