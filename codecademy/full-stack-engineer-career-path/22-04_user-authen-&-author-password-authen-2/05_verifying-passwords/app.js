2ac6cc9bc9a64e70c20259192c3327acb4336286
hkmarcoot <hkmarcoot@gmail.com>
2024-12-19 21:49:53 +0000

    Add Codecademy Example - Verifying Passwords

---
+++ codecademy/full-stack-engineer-career-path/22-04_user-authen-&-author-password-authen-2/05_verifying-passwords/app.js
@@ -0,0 +1,12 @@
+const bcrypt = require("bcrypt");
+
+// Create your password comparison function below:
+const comparePasswords = async (password, hash) => {
+  try {
+    const matchFound = await bcrypt.compare(password, hash);
+    return matchFound;
+  } catch (err) {
+    console.log(err);
+  }
+  return false;
+};
