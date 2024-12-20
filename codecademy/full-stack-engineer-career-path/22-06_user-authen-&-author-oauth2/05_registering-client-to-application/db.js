25bac505ec2b11462012811c727d1dc4db31080e
hkmarcoot <hkmarcoot@gmail.com>
2024-12-20 20:43:48 +0000

    Add Codecademy Example - Registering Client to Application

---
+++ codecademy/full-stack-engineer-career-path/22-06_user-authen-&-author-oauth2/05_registering-client-to-application/db.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // Add confidential clients[]
+  confidentialClients: [
+    {
+      clientId: "codecademy",
+      clientSecret: "codec@demy",
+      grants: ["client_credentials"],
+    },
+  ],
+  tokens: [],
+
+  // Add tokens[]
+};
