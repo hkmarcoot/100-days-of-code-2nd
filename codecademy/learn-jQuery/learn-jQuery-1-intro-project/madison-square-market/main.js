ee6fa4545d5b3408decb1c8f43a5eee93d0a9308
hkmarcoot <hkmarcoot@gmail.com>
2025-02-08 19:57:39 +0000

    Add Codecademy Project - Madison Square Market

---
+++ codecademy/learn-jQuery/learn-jQuery-1-intro-project/madison-square-market/main.js
@@ -0,0 +1,22 @@
+$(document).ready(() => {
+  $("#cart").on("click", () => {
+    $("#cartMenu").show();
+  });
+  $("#cart").on("mouseleave", () => {
+    $("#cartMenu").hide();
+  });
+
+  $("#account").on("click", () => {
+    $("#accountMenu").show();
+  });
+  $("#account").on("mouseleave", () => {
+    $("#accountMenu").hide();
+  });
+
+  $("#help").on("click", () => {
+    $("#helpMenu").show();
+  });
+  $("#help").on("mouseleave", () => {
+    $("#helpMenu").hide();
+  });
+});
