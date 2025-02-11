2d3d3bcb2661e673865dcadd76de6b5422279017
hkmarcoot <hkmarcoot@gmail.com>
2025-02-11 21:40:34 +0000

    Add Codecademy Project - Block Letters

---
+++ codecademy/learn-python-3/01-02_hello-world-project-1/block-letters/script.py
@@ -0,0 +1,13 @@
+# Jeremy Barbosa
+# Fun Fact: I can play 3 musical instruments
+name = """
+JJJJJ BBBB
+  J   B   B
+  J   B   B
+  J   BBBB
+J J   B   B
+J J   B   B
+ JJ   BBBB
+"""
+
+print(name)
