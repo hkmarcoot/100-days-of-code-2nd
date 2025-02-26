8a57eb5811c4e536e8d686e2d84482e6703266f3
hkmarcoot <hkmarcoot@gmail.com>
2025-02-26 22:17:19 +0000

    Add Codecademy Project - Planting A Tree

---
+++ codecademy/learn-java/01-02_hello-world-project/planting-a-tree/Tree.java
@@ -0,0 +1,14 @@
+// Define your tree class in this file!
+public class Tree {
+    public static void main(String[] args) {
+      // The main method executes the tasks of the class
+      System.out.println("Hey there, I’m Mar and I’m learning to code in Java!"); 
+      System.out.println("I’m going to plant a tree today!");   
+      System.out.println("  *  ");
+      System.out.println(" *** ");
+      System.out.println(" *** ");
+      System.out.println("  *  ");
+      System.out.println("  *  ");   
+
+    }
+  }
