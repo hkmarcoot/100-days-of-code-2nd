1010be6cd645600bd2792594e36bd6d16dcba053
hkmarcoot <hkmarcoot@gmail.com>
2025-03-03 16:51:07 +0000

    Add Examples & Notes For Java String Methods

---
+++ codecademy/learn-java/07-01_string-methods/04_equals()/Password.java
@@ -0,0 +1,14 @@
+public class Password {
+  
+    public static void main(String[] args) {
+      
+      String password = "correcthorsebatterystaple";
+      // String password2 = "correcthorsebatterystaple";
+      // String password3 = "correcthorsebatterystaple3";
+      
+      // Write the code:
+      System.out.println(password.equals("correcthorsebatterystaple3"));
+      // System.out.println(password.equals(password3));
+    }
+    
+  }
