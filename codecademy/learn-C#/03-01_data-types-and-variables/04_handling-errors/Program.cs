5ef1f150d2c5bba3bcada02d882472a4139611de
hkmarcoot <hkmarcoot@gmail.com>
2025-03-06 21:59:37 +0000

    Add Examples & Notes For C# Hello World and Data Types and Variables

---
+++ codecademy/learn-C#/03-01_data-types-and-variables/04_handling-errors/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace BugSquasher
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double number = 38498.3222;
+
+      string dinosaur = "Barney";
+
+      double lockValue = 293.000;
+
+      bool yes = true;
+
+      string band = "The Low Anthem";
+
+    }
+  }
+}
---
+++ codecademy/learn-C#/03-01_data-types-and-variables/06_review/notes.md
@@ -0,0 +1,6 @@
+Topics covered include:
+
+- C# built-in data types, including int, double, char, string, and bool
+- how to create, name, and use variables
+- common errors you might encounter
+- converting data types from one to another
