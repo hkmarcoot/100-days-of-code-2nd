22b9bae424d2bcb44f0de8382a8c4b78dbb4cf98
hkmarcoot <hkmarcoot@gmail.com>
2025-03-06 23:12:21 +0000

    Add Codecademy Project - Money Maker

---
+++ codecademy/learn-C#/04-02_project/money-maker/Program.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace MoneyMaker
+{
+  class MainClass
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to Money Maker!");
+      Console.WriteLine("Enter an amount to convert to coins: ");
+      string totalAsString = Console.ReadLine();
+      double totalAsDouble = Convert.ToDouble(totalAsString);
+      Console.WriteLine($"{totalAsDouble} cents is equal to ...");
+      int goldValue = 10;
+      int silverValue = 5;
+      double goldCoins = Math.Floor(totalAsDouble / goldValue);
+      double remainder = totalAsDouble % goldValue;
+      double silverCoins = Math.Floor(remainder / silverValue);
+      remainder = remainder % silverValue;
+      Console.WriteLine($"Gold coins: {goldCoins}");
+      Console.WriteLine($"Silver coins: {silverCoins}");
+      Console.WriteLine($"Bronze coins: {remainder}");
+    }
+  }
+}
