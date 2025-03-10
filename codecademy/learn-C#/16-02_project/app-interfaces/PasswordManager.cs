d2a2655a08a633c1761baeb7c00eff9f824f6b67
hkmarcoot <hkmarcoot@gmail.com>
2025-03-10 23:08:18 +0000

    Add Codecademy Project - App Interfaces

---
+++ codecademy/learn-C#/16-02_project/app-interfaces/PasswordManager.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace SavingInterface
+{
+  class PasswordManager : IDisplayable, IResetable
+  {
+    private string Password
+    { get; set; }
+
+    public bool Hidden
+    { get; private set; }
+
+    public PasswordManager(string password, bool hidden)
+    {
+      Password = password;
+      Hidden = hidden;
+    }
+
+    public void Display(){
+      if(Hidden == false){
+        Console.WriteLine(Password);
+      } else if (Hidden == true){
+        Console.WriteLine(new string('*', Password.Length));
+      }  
+    }
+
+    public void Reset(){
+      Password = "";
+      Hidden = false;
+    }
+  }
+}
